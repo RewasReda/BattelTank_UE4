a71735ba049a9521415bc4ce82e7fae4a6db9fcb
RewasReda <rewasreda2015@gmail.com>
2019-06-30 08:27:06 +0200

    Project Creation

---
+++ BattelTank/Source/BattelTank/BattelTankGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattelTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTELTANK_API ABattelTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
